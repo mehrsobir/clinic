4b6023fec85e62019a5f108337dc2aa192434651
Mehr <mehrsobir2@gmail.com>
2022-08-27 11:38:23 +0500

    my work2

--- clinic/settings.py
+++ clinic/settings.py
@@ -127,6 +127,6 @@ STATICFILES_DIRS = [
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL = '/images/'
 
+MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
